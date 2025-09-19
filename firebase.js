ee779bedaf8ace9603a9a2287d4f841c33e27f72
taladudu532-stack <taladudu532@gmail.com>
2025-09-19 10:33:14 +0300

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,21 +1,24 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+<!-- Firebase App (core) -->
+<script src="https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js"></script>
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyASQomgcUm0LNNM8ABYxq-vqz7BYpO-eg4",
-  authDomain: "talabet-89fce.firebaseapp.com",
-  projectId: "talabet-89fce",
-  storageBucket: "talabet-89fce.firebasestorage.app",
-  messagingSenderId: "767205554053",
-  appId: "1:767205554053:web:32cc62707e62aca28b76ec",
-  measurementId: "G-FKLT279RDX"
-};
+<!-- Firebase Analytics (optional) -->
+<script src="https://www.gstatic.com/firebasejs/10.5.0/firebase-analytics.js"></script>
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+<!-- Firebase Firestore (for coin tracking) -->
+<script src="https://www.gstatic.com/firebasejs/10.5.0/firebase-firestore.js"></script>
+
+<script>
+  const firebaseConfig = {
+    apiKey: "AIzaSyASQomgcUm0LNNM8ABYxq-vqz7BYpO-eg4",
+    authDomain: "talabet-89fce.firebaseapp.com",
+    projectId: "talabet-89fce",
+    storageBucket: "talabet-89fce.firebasestorage.app",
+    messagingSenderId: "767205554053",
+    appId: "1:767205554053:web:32cc62707e62aca28b76ec",
+    measurementId: "G-FKLT279RDX"
+  };
+
+  const app = firebase.initializeApp(firebaseConfig);
+  const analytics = firebase.analytics();
+  const db = firebase.firestore();
+</script>
